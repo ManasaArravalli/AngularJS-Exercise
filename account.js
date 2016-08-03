5e3a4c278a397c89fd1903aa0561d5556f5c7a38
Manasa Arravalli <a.manasareddy@gmail.com>
2016-08-03 00:58:53 -0500

    Added files

---
+++ account.js
@@ -0,0 +1,103 @@
+angular.module('example', ['ui', 'ngRoute'])
+ .controller('AccountStructureController', ['$scope', 'DataService', function($scope, DataService) {
+  $scope.accountStructurList = [];
+  //Sample list for structure
+  $scope.accountStructurList = DataService.getAccountStructureData();
+  $scope.user = {};
+  $scope.total = 0;
+  $scope.parentAccountStructure = [{
+   id: 1,
+   accountNumber: "123",
+   accountDescription: "abc",
+   balance: "4500"
+  }, {
+   id: 2,
+   accountNumber: "2345",
+   accountDescription: "def",
+   balance: "5500"
+  }]
+  $scope.accountTypes = DataService.getAccountTypeData();
+  //Adding account structure
+  $scope.addAccountStructure = function(user) {
+   // $scope.user.accountChildDescription=user.accountDescription;
+   
+   $scope.total = $scope.total + parseInt(user.balance);
+   // $scope.accountStructurList.push(user);
+   DataService.setAccountStructureData(user);
+   $scope.accountStructurList = DataService.getAccountStructureData();
+   $scope.reset();
+  };
+
+  $scope.reset = function() {
+   $scope.user = '';
+  };
+
+  $scope.deleteAccountStructure = function(acc) {
+   $scope.accountStructurList.splice($scope.accountStructurList.indexOf(acc), 1);
+  };
+
+
+ }]);
+
+angular.module('example').controller('AccountTypeController', ['$scope', '$window', 'DataService', function($scope, $window, DataService) {
+ $scope.accountTypeList = [];
+
+
+ $scope.parentAccountStructure = DataService.getAccountStructureData();
+
+ $scope.addAccountType = function(account) {
+  // $scope.user.accountChildDescription=user.accountDescription;
+  $scope.accountTypeList.push(account);
+  DataService.setAccountTypeData(account);
+  $scope.reset();
+ };
+
+ $scope.reset = function() {
+  $scope.account = '';
+ };
+
+ $scope.deleteAccountStructure = function(acc) {
+  $scope.accountTypeList.splice($scope.accountTypeList.indexOf(acc), 1);
+ };
+
+
+}]);
+//Using services, push to the list structure
+angular.module('example').factory('DataService', function() {
+
+ var object = {};
+ var accountTypeList = [];
+ var accountStructurList = [];
+ object.setAccountStructureData = function(obj) {
+  accountStructurList.push(obj);
+ }
+
+ object.getAccountStructureData = function() {
+  return accountStructurList;
+ }
+ object.setAccountTypeData = function(obj) {
+  accountTypeList.push(obj);
+ }
+
+ object.getAccountTypeData = function() {
+  return accountTypeList;
+ }
+ return object;
+});
+// Routing 
+angular.module('example').config(function($routeProvider) {
+ $routeProvider
+
+ // route for the home page
+  .when('/', {
+  templateUrl: 'accountStructure.html',
+  controller: 'AccountStructureController'
+ })
+
+ // route for the add account type page
+ .when('/accountType', {
+  templateUrl: 'accountType.html',
+  controller: 'AccountTypeController'
+ });
+
+});
